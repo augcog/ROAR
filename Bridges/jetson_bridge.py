79c8eb38fd2188bb83981d8c35c11268a92fe83c
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-11-09 21:05:57 -0800

    erR

--- Bridges/jetson_bridge.py
+++ Bridges/jetson_bridge.py
@@ -76,7 +76,7 @@ class JetsonBridge(Bridge):
                 ),
                 rotation=Rotation(
                     roll=-source.roll,
-                    pitch=source.pitch,
+                    pitch=source.pitch + 70,
                     yaw=-source.yaw
                 ),
                 velocity=Vector3D(
