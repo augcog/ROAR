df764673ee4505bdf2f96506b10ee46051377221
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-12-01 21:53:03 -0800

    pid rl agent done

--- runner_sim.py
+++ runner_sim.py
@@ -1,3 +1,9 @@
+import warnings
+import logging
+
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+logging.getLogger("numpy").setLevel(logging.ERROR)
+warnings.filterwarnings('ignore')
 import logging
 import numpy as np
 import warnings
@@ -8,7 +14,7 @@ from ROAR.agent_module.pure_pursuit_agent import PurePursuitAgent
 # from ROAR.agent_module.point_cloud_agent import PointCloudAgent
 from ROAR.configurations.configuration import Configuration as AgentConfig
 from ROAR.agent_module.special_agents.waypoint_generating_agent import WaypointGeneratigAgent
-from ROAR.agent_module.pid_agent import PIDAgent
+from ROAR.agent_module.rl_testing_pid_agent import RLPIDAgent
 
 
 def main():
@@ -20,7 +26,7 @@ def main():
                                npc_agent_class=PurePursuitAgent)
     try:
         my_vehicle = carla_runner.set_carla_world()
-        agent = PIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
+        agent = RLPIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
         carla_runner.start_game_loop(agent=agent, use_manual_control=False)
     except Exception as e:
         logging.error(f"Something bad happened during initialization: {e}")
