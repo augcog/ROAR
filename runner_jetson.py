92c22c100d8ae968bf294402002ce3b204230e91
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-05-01 16:56:20 -0700

    im too lazy to type all these, runing a script

--- runner_jetson.py
+++ runner_jetson.py
@@ -30,9 +30,9 @@ def main():
             prepare(jetson_config=jetson_config)
         except Exception as e:
             logging.error(f"Ignoring Error during setup: {e}")
-        agent = RecordingAgent(vehicle=Vehicle(), agent_settings=agent_config, should_init_default_cam=False)
+        agent = PIDAgent(vehicle=Vehicle(), agent_settings=agent_config, should_init_default_cam=False)
         jetson_runner = JetsonRunner(agent=agent, jetson_config=jetson_config)
-        jetson_runner.start_game_loop(use_manual_control=True)
+        jetson_runner.start_game_loop(use_manual_control=False)
     except Exception as e:
         print(f"Something bad happened {e}")
 
