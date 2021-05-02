040803502e77bf581a7184b39a5ce6e34852f387
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-05-01 19:52:17 -0700

    im too lazy to type all these, runing a script

--- misc/track_visualizer.py
+++ misc/track_visualizer.py
@@ -95,7 +95,7 @@ def visualize_tracks_together(data_dir: Path = Path("./data"), width: int = 1000
 
 
 if __name__ == "__main__":
-    track_data: List[List[float]] = read_txt(Path("rfs_waypoints.txt"))
-    visualize_track_data(track_data=track_data)
+    # track_data: List[List[float]] = read_txt(Path("rfs_waypoints.txt"))
+    # visualize_track_data(track_data=track_data)
     # visualize_tracks(regex="trajectory_log*")
     visualize_tracks_together(data_dir=Path("."), regex="rfs_waypoints_*.txt")
