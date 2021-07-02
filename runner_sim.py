c32fa9ae6754d13fe2b8413f33da43877d85a939
moonwonlee <moonwonlee@berkeley.edu>
2021-07-02 13:25:44 -0700

    looking good

--- runner_sim.py
+++ runner_sim.py
@@ -35,58 +35,70 @@ def main():
     pitstop.set_carla_sync_mode(False)
     pitstop.set_autopilot_mode(True)
     pitstop.set_car_color(CarlaCarColor(r = 255,g = 200,b = 00,a = 255))
-    pitstop.set_num_laps(num=7)
+    pitstop.set_num_laps(num=1)
     pitstop.set_output_data_folder_path("./data/output")
     pitstop.set_output_data_file_name(time.strftime("%Y%m%d-%H%M%S-") + "map-waypoints")
     pitstop.set_max_speed(speed = 200)
     pitstop.set_target_speed(speed = 30)
     print(agent_config.target_speed, " target speed")
     pitstop.set_steering_boundary(boundary = (-1.0, 1.0))
-    pitstop.set_throttle_boundary(boundary = (0, 1))
-    pitstop.set_waypoints_look_ahead_values(values={"10":5, "20": 5, "30": 5, "50": 5, "60": 10, "70": 10, "80": 20, "100": 50, "120": 50, "150": 50, "180": 50})
+    pitstop.set_throttle_boundary(boundary = (0, 0.5))
+    pitstop.set_waypoints_look_ahead_values(values={
+                                                    "60": 5,
+                                                    "80": 10,
+                                                    "120": 20,
+                                                    "180": 50})
     pid_value = {
-                    'longitudinal_controller': {
-                        '40': {
-                            'Kp': 0.8, 'Kd': 0.2, 'Ki': 0.1
-                        }, 
-                        '60': {
-                            'Kp': 0.5, 'Kd': 0.2, 'Ki': 0.1
-                        }, 
-                        '150': {
-                            'Kp': 0.2, 'Kd': 0.1, 'Ki': 0.1
-                        }
-                    },   
-                    'latitudinal_controller': {
-                        '60': {
-                            'Kp': 0.8, 'Kd': 0.1, 'Ki': 0.1
-                        }, 
-                        '100': {
-                            'Kp': 0.6, 'Kd': 0.2, 'Ki': 0.1
-                        }, 
-                        '150': {
-                            'Kp': 0.5, 'Kd': 0.2, 'Ki': 0.1
-                        }
+                    "longitudinal_controller": {
+                        "40": {
+                            "Kp": 0.8,
+                            "Kd": 0.2,
+                            "Ki": 0
+                        },
+                        "60": {
+                            "Kp": 0.5,
+                            "Kd": 0.2,
+                            "Ki": 0
+                        },
+                        "150": {
+                            "Kp": 0.2,
+                            "Kd": 0.1,
+                            "Ki": 0.1
+                            }
+                    },
+                    "latitudinal_controller": {
+                        "60": {
+                            "Kp": 0.8,
+                            "Kd": 0.1,
+                            "Ki": 0.1
+                        },
+                        "100": {
+                            "Kp": 0.6,
+                            "Kd": 0.2,
+                            "Ki": 0.1
+                        },
+                        "150": {
+                            "Kp": 0.5,
+                            "Kd": 0.2,
+                            "Ki": 0.1
+                            }
                     }
                 }
     pitstop.set_pid_values(pid_value)
 
     """Passing configurations to Carla and Agent"""
-
     carla_runner = CarlaRunner(carla_settings=carla_config, # ROAR Academy: fine
                                agent_settings=agent_config, # ROAR Academy: fine
                                npc_agent_class=PurePursuitAgent)
     try:
         my_vehicle = carla_runner.set_carla_world()
-        # agent = RecordingAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        # agent = ForwardOnlyAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        # agent = PIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        # agent = OccuMapDemoDrivingAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        # agent = PIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        # agent = RLPIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
-        agent = WaypointGeneratigAgent(vehicle=my_vehicle, agent_settings=agent_config)
+
+        agent = PIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
+        # agent = WaypointGeneratigAgent(vehicle=my_vehicle, agent_settings=agent_config)
         
-        carla_runner.start_game_loop(agent=agent, use_manual_control=False)
-        carla_runner.start_game_loop(agent=agent, use_manual_control=True) # for WaypointGeneratingAgent
+        carla_runner.start_game_loop(agent=agent, use_manual_control=False) # for PIDAgent
+        # carla_runner.start_game_loop(agent=agent, use_manual_control=True) # for WaypointGeneratingAgent
+    
     except Exception as e:
         logging.error(f"Something bad happened during initialization: {e}")
         carla_runner.on_finish()
