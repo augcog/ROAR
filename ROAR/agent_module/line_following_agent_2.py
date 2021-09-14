016b1bcfb4617697787b909b118cc6bfe55a4aeb
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-09-13 19:10:12 -0700

    ready to do error at multi location

--- ROAR/agent_module/line_following_agent_2.py
+++ ROAR/agent_module/line_following_agent_2.py
@@ -8,7 +8,7 @@ from ROAR.control_module.simple_pid_controller import SimplePIDController
 from ROAR.perception_module.simple_line_detector import LineDetector
 from ROAR.planning_module.local_planner.rgb_pixel_planner import RGBPixelPlanner
 from collections import deque
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 
 class LineFollowingAgent(Agent):
@@ -38,41 +38,11 @@ class LineFollowingAgent(Agent):
             rgb_data: np.ndarray = cv2.resize(self.front_rgb_camera.data.copy(),
                                               dsize=(depth_data.shape[1], depth_data.shape[0]))
             # find the lane
-            y = rgb_data.shape[0] - 10
-            lane_x = []
-            mask = cv2.inRange(src=rgb_data, lowerb=self.lower_range, upperb=self.upper_range)
-            kernel = np.ones((5, 5), np.uint8)
-            mask = cv2.erode(mask, kernel, iterations=1)
-            mask = cv2.dilate(mask, kernel, iterations=1)
+            error_at_10 = self.find_error_at(rgb_data=rgb_data, y_offset=10, error_scaling=self.error_scaling)
+            if error_at_10 is None:
+                return self.execute_prev_command()
 
-            for x in range(0, rgb_data.shape[1], 5):
-                if mask[y][x] > 0:
-                    lane_x.append(x)
-
-            if len(lane_x) == 0:
-                # no lane found, execute the previous control with a decaying factor
-                if np.average(self.prev_steerings) < 0:
-                    self.vehicle.control.steering = -1
-                else:
-                    self.vehicle.control.steering = 1
-                self.prev_steerings.append(self.vehicle.control.steering)
-                self.vehicle.control.throttle = 0.2
-                self.logger.info(f"No Lane found, executing discounted prev command: {self.vehicle.control}")
-                return self.vehicle.control
-
-            # if lane is found
-            avg_x = int(np.average(lane_x))
-
-            # find error
-            center_x = rgb_data.shape[1] // 2
-
-            error = avg_x - center_x
-            # we want small error to be almost ignored, only big errors matter.
-            for e, scale in self.error_scaling:
-                if abs(error) <= e:
-                    error = error * scale
-                    break
-
+            error = error_at_10
             self.kwargs["lat_error"] = error
             self.vehicle.control = self.controller.run_in_series()
             self.prev_steerings.append(self.vehicle.control.steering)
@@ -80,3 +50,43 @@ class LineFollowingAgent(Agent):
         else:
             # image feed is not available yet
             return VehicleControl()
+
+    def find_error_at(self, rgb_data, y_offset, error_scaling) -> Optional[float]:
+        y = rgb_data.shape[0] - y_offset
+        lane_x = []
+        mask = cv2.inRange(src=rgb_data, lowerb=self.lower_range, upperb=self.upper_range)
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+
+        for x in range(0, rgb_data.shape[1], 5):
+            if mask[y][x] > 0:
+                lane_x.append(x)
+
+        if len(lane_x) == 0:
+            return None
+
+        # if lane is found
+        avg_x = int(np.average(lane_x))
+
+        # find error
+        center_x = rgb_data.shape[1] // 2
+
+        error = avg_x - center_x
+        # we want small error to be almost ignored, only big errors matter.
+        for e, scale in error_scaling:
+            if abs(error) <= e:
+                error = error * scale
+                break
+        return error
+
+    def execute_prev_command(self):
+        # no lane found, execute the previous control with a decaying factor
+        if np.average(self.prev_steerings) < 0:
+            self.vehicle.control.steering = -1
+        else:
+            self.vehicle.control.steering = 1
+        self.prev_steerings.append(self.vehicle.control.steering)
+        self.vehicle.control.throttle = 0.2
+        self.logger.info(f"No Lane found, executing discounted prev command: {self.vehicle.control}")
+        return self.vehicle.control
