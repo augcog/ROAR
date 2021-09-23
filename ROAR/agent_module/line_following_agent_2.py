6fab9997e402683a6cc72aaf359570754aec39ad
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-09-22 20:48:18 -0700

    avg depth

--- ROAR/agent_module/line_following_agent_2.py
+++ ROAR/agent_module/line_following_agent_2.py
@@ -34,6 +34,7 @@ class LineFollowingAgent(Agent):
             cv2.imshow("depth", self.front_depth_camera.data)
             cropped_depth = self.front_depth_camera.data[150:, :]
             avg_depth = np.average(cropped_depth)
+            print(avg_depth)
             if avg_depth < 1:
                 return VehicleControl(throttle=0, steering=0)
             # im2 = self.rgb2ycbcr(rgb_data)
