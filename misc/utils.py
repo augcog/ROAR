8d5feac0d25b2929b46f8f83a3f5a912b7799379
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-10-06 08:38:03 -0700

    more tuning of data required

--- misc/utils.py
+++ misc/utils.py
@@ -1,4 +1,7 @@
 import argparse
+import cv2
+import numpy as np
+from typing import Tuple
 
 
 def str2bool(v):
@@ -10,3 +13,32 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def depth2colorjet(depth: np.ndarray) -> np.ndarray:
+    """
+    Given a 2d numpy array of floats ranging from 0 - 1,
+    turn it into a uint8 2d numpy array with jet color mapping
+    """
+    depth = -1 * np.log(depth)
+    depth = (depth / np.max(depth) * 255).astype(np.uint8)
+    rgb = cv2.applyColorMap(depth, cv2.COLORMAP_JET)
+    return rgb
+
+
+def flip(img: np.ndarray, steering_angle: float) -> Tuple[np.ndarray, float]:
+    return np.fliplr(img), -steering_angle
+
+
+def crop_roi(img: np.ndarray, min_x: int, min_y: int, max_x: int, max_y: int):
+    return img[min_x:max_x, min_y:max_y]
+
+
+def random_flip(image, steering_angle):
+    """
+    Randomly flipt the image left <-> right, and adjust the steering angle.
+    """
+    if np.random.rand() < 0.5:
+        image = cv2.flip(image, 1)
+        steering_angle = -steering_angle
+    return image, steering_angle
