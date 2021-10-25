8f1a7c36e8ccf35cf5f8720fd430abe4a9427418
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-10-25 01:30:48 -0700

    submoduleupdated
    qr code scanning added

--- runner_ios.py
+++ runner_ios.py
@@ -11,12 +11,55 @@ from ROAR.utilities_module.vehicle_models import Vehicle
 import logging
 import argparse
 from misc.utils import str2bool
+from ROAR.utilities_module.utilities import get_ip
+import qrcode
+import cv2
+import numpy as np
+import socket
+import json
+
 
 
 class mode_list(list):
     # list subclass that uses lower() when testing for 'in'
     def __contains__(self, other):
         return super(mode_list, self).__contains__(other.lower())
+
+
+def showIPUntilAck():
+    img = np.array(qrcode.make(f"{get_ip()}").convert('RGB'))
+    success = False
+    addr = None
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    try:
+        s.bind((get_ip(), 8008))
+        s.settimeout(1)
+        while True:
+            try:
+                s.listen()
+
+                cv2.imshow("Scan this code to connect to phone", img)
+                k = cv2.waitKey(1) & 0xff
+                if k == ord('q') or k == 27:
+                    s.close()
+                    break
+                conn, addr = s.accept()
+                addr = addr[0]
+                if conn:
+                    s.close()
+                    success = True
+                    break
+            except socket.timeout as e:
+                logging.info(f"Please tap on the ip address to scan QR code. {e}")
+    except Exception as e:
+        logging.error(f"Unable to bind socket: {e}")
+    finally:
+        s.close()
+        cv2.destroyWindow("Scan this code to connect to phone")
+    return success, addr
 
 
 if __name__ == '__main__':
@@ -29,19 +72,26 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--auto", type=str2bool, default=False, help="True to use auto control")
     parser.add_argument("-m", "--mode", choices=choices, help="AR or VR", default="vr")
+    parser.add_argument("-r", "--reconnect", type=str2bool, default=True, help="Scan QR code to attach phone to PC")
     args = parser.parse_args()
 
     try:
-        agent_config = AgentConfig.parse_file(
-            Path("ROAR/configurations/iOS/iOS_agent_configuration.json")
-        )
-        ios_config = iOSConfig.parse_file(
-            Path("ROAR_iOS/configurations/ios_config.json")
-        )
+        agent_config_file_path = Path("ROAR/configurations/iOS/iOS_agent_configuration.json")
+        ios_config_file_path = Path("ROAR_iOS/configurations/ios_config.json")
+        agent_config = AgentConfig.parse_file(agent_config_file_path)
+        ios_config: iOSConfig = iOSConfig.parse_file(ios_config_file_path)
         ios_config.ar_mode = True if args.mode == "ar" else False
 
-        agent = ForwardOnlyAgent(vehicle=Vehicle(), agent_settings=agent_config, should_init_default_cam=True)
-        ios_runner = iOSRunner(agent=agent, ios_config=ios_config)
-        ios_runner.start_game_loop(auto_pilot=args.auto)
+        success = False
+        if args.reconnect:
+            success, addr = showIPUntilAck()
+            if success:
+                ios_config.ios_ip_addr = addr
+                json.dump(ios_config.dict(), ios_config_file_path.open('w'), indent=4)
+
+        if success or args.reconnect is False:
+            agent = ForwardOnlyAgent(vehicle=Vehicle(), agent_settings=agent_config, should_init_default_cam=True)
+            ios_runner = iOSRunner(agent=agent, ios_config=ios_config)
+            ios_runner.start_game_loop(auto_pilot=args.auto)
     except Exception as e:
         print(f"Something bad happened: {e}")
