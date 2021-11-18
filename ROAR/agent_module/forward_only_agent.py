a46d2212e458055f2d6e72e0ea3f44101d941237
Michael Wu <wuxiaohua1011@gmail.com>
2021-11-18 09:04:09 -0800

    prep for today's test

--- ROAR/agent_module/forward_only_agent.py
+++ ROAR/agent_module/forward_only_agent.py
@@ -4,21 +4,21 @@ from ROAR.utilities_module.vehicle_models import Vehicle, VehicleControl
 from ROAR.configurations.configuration import Configuration as AgentConfig
 import cv2
 from collections import deque
-
+import numpy as np
 
 class ForwardOnlyAgent(Agent):
     def __init__(self, vehicle: Vehicle, agent_settings: AgentConfig, **kwargs):
         super().__init__(vehicle, agent_settings, **kwargs)
         self.log = deque(maxlen=100)
+        self.tmp = 1
 
     def run_step(self, sensors_data: SensorsData, vehicle: Vehicle) -> VehicleControl:
         super().run_step(sensors_data=sensors_data, vehicle=vehicle)
         steering = 0
-        return VehicleControl(throttle=0.2)
         # return VehicleControl(brake=True)
-        # if self.time_counter > 50:
-        #     return VehicleControl(throttle=0, steering=steering, brake=True)
-        # else:
-        #     return VehicleControl(throttle=-0.5, steering=steering, brake=False)
+        if self.time_counter > 50:
+            return VehicleControl(throttle=0, steering=steering, brake=True)
+        else:
+            return VehicleControl(throttle=-0.5, steering=steering, brake=False)
 
 
