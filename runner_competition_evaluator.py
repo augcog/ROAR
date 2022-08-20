58a8dde838bdc8ebeba2275c50214820be070665
Vancony Whale <gamevancony@gmail.com>
2022-08-20 12:08:01 -0800

    fixed simulation time name for evaluator

--- runner_competition_evaluator.py
+++ runner_competition_evaluator.py
@@ -25,7 +25,7 @@ def compute_score(carla_runner: CarlaRunner) -> Tuple[float, int, int]:
     """
     # time_elapsed: float = carla_runner.end_simulation_time - carla_runner.start_simulation_time
     # use simulation time, not real time
-    time_elapsed: float = carla_runner.end_sim_time - carla_runner.start_sim_time
+    time_elapsed: float = carla_runner.end_simulation_time - carla_runner.start_simulation_time
     num_collision: int = carla_runner.agent_collision_counter
     laps_completed = 0 if carla_runner.completed_lap_count < 0 else carla_runner.completed_lap_count
 
